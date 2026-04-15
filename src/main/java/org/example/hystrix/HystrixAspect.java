dd00f67f5d4de5c56484a9d165723cd43706e11a
TBYBY <1051733980@qq.com>
2026-04-15 10:12:19 +0800

    fix:修改配置读取方式为配置文件读取,非配置类读取

--- src/main/java/org/example/hystrix/HystrixAspect.java
+++ src/main/java/org/example/hystrix/HystrixAspect.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 import java.lang.reflect.Method;
@@ -12,16 +13,11 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 @Aspect
-@EnableConfigurationProperties()
+@ConditionalOnProperty(name="hystrix.enable",havingValue = "true")
 public class HystrixAspect {
 
     @Pointcut("@annotation(org.example.hystrix.HystrixTest)")
     public void getHystrix(){}
-
-    /**
-     * 定义缓存，将 hystrix 的值按照 <Method, 存入缓存
-     */
-    private static final ConcurrentHashMap<String, HystrixTest> hystrixValueMap = new ConcurrentHashMap<>();
 
     @Around("getHystrix()")
     public Object process(ProceedingJoinPoint joinPoint) throws Throwable{
@@ -31,8 +27,6 @@ public class HystrixAspect {
         Method method = signature.getMethod();
         HystrixTest hystrix = method.getAnnotation(HystrixTest.class);
         key = hystrix.name();
-        // 判断当前方法是否将 Hystrix 参数存入,使用原子更新,以免多线程并发冲突
-        hystrixValueMap.putIfAbsent(key, hystrix);
 
         return result;
     }
